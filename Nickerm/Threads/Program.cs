02b1b029fa76e675b8d013a86f97207215f30147
Nickerm <nick.ermolenkov@gmail.com>
2021-05-19 15:22:30 +0300

    feat: add threads solution

---
+++ Nickerm/Threads/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    await Task.Run(() => Worker.DoWork(i));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+    }
+}
