df267812aaf4f59f82a7338a3af98765319ed5a6
Nickerm <nick.ermolenkov@gmail.com>
2021-05-28 12:33:04 +0300

    refactor: refactor code

--- Nickerm/Streams/MemoryStream/MemoryStreamReadWrite.cs
+++ Nickerm/Streams/MemoryStream/MemoryStreamReadWrite.cs
@@ -21,14 +21,17 @@ namespace Streams
         {
             try
             {
-                byte[] textArray = uniEncoding.GetBytes(_text);
-                using (MemoryStream ms = new MemoryStream(textArray))
+                var textArray = uniEncoding.GetBytes(_text);
+
+                using (var ms = new MemoryStream(textArray))
                 {
-                    using (FileStream fs = new FileStream(_path, FileMode.Create, FileAccess.Write))
+                    using (var fs = new FileStream(_path, FileMode.Create, FileAccess.Write))
                     {
-                        byte[] bytes = new byte[ms.Length];
+                        var bytes = new byte[ms.Length];
+
                         ms.Read(bytes, 0, (int)ms.Length);
                         fs.Write(bytes, 0, bytes.Length);
+
                         Console.WriteLine("Done write");
                     }
                 }
@@ -43,15 +46,18 @@ namespace Streams
         {
             try
             {
-                using (MemoryStream ms = new MemoryStream())
+                using (var ms = new MemoryStream())
                 {
-                    using (FileStream fs = new FileStream(_path, FileMode.Open, FileAccess.Read))
+                    using (var fs = new FileStream(_path, FileMode.Open, FileAccess.Read))
                     {
-                        byte[] bytes = new byte[fs.Length];
+                        var bytes = new byte[fs.Length];
+
                         fs.Read(bytes, 0, (int)fs.Length);
                         ms.Write(bytes, 0, (int)fs.Length);
+
                         var charArray = new char[uniEncoding.GetCharCount(bytes, 0, bytes.Length)];
                         uniEncoding.GetDecoder().GetChars(bytes, 0, bytes.Length, charArray, 0);
+
                         Console.WriteLine(charArray);
                         Console.WriteLine("Done read");
                     }
