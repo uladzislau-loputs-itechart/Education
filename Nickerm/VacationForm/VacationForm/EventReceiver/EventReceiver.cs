1f5a13e2df783910a4f2d08e431033b098ff07c0
Nickerm <nick.ermolenkov@gmail.com>
2021-07-02 17:48:06 +0300

    init: add vacation form solution

---
+++ Nickerm/VacationForm/VacationForm/EventReceiver/EventReceiver.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.Web;
+using System.Net.Mail;
+using System.Net;
+
+namespace VacationForm.EventReceiver
+{
+
+    public class AddEventReceiver : SPItemEventReceiver
+    {
+
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            try
+            {
+                //MailMessage mail = new MailMessage();
+                //mail.From = new MailAddress("YourEmailID@gmail.com");
+                //mail.Bcc.Add("bccEmail@gmail.com");
+                //mail.CC.Add("sampleEmail@ymail.com");
+                //mail.To.Add("nick.ermolenkov@gmail.com");
+                //mail.Subject = "Test Mail";
+                //mail.Body = "Hi...!!! This is a test mail.";
+                //mail.IsBodyHtml = true;
+                //SmtpClient smtpClient = new SmtpClient();
+                //smtpClient.Host = properties.Web.Site.WebApplication.OutboundMailServiceInstance.Server.Address; 
+                //smtpClient.Port = 25;
+                //smtpClient.UseDefaultCredentials = false;
+                ////smtpClient.Credentials = new System.Net.NetworkCredential("YourEmailID@gmail.com", "YourPassword");
+                //smtpClient.EnableSsl = true;
+                //ServicePointManager.ServerCertificateValidationCallback = delegate
+                //{
+                //    return true;
+                //};
+                //smtpClient.Send(mail);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = properties.Web.Site.WebApplication.OutboundMailServiceInstance.Server.Address;
+                MailMessage mailMessage = new MailMessage("sharavik1992@gmail.com", "nick.ermolenkov@gmail.com");
+                mailMessage.Subject = "Test Mail";
+
+                mailMessage.Body = "Hi...!!! This is a test mail.";
+                mailMessage.IsBodyHtml = true;
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+
+        }
+
+        //public override void ItemAdded(SPItemEventProperties properties)
+        //{
+        //    base.ItemAdded(properties);
+        //    string Siteurl = properties.ListItem.Web.Url;
+        //    using (SPSite site = new SPSite(Siteurl))
+        //    {
+        //        using (SPWeb web = site.OpenWeb())
+        //        {
+        //            web.AllowUnsafeUpdates = true;
+        //            SPListItem currentItem = properties.ListItem;
+        //            currentItem["User_x0020_email"] = "email";
+        //            var success = SPUtility.SendEmail(web, true, true, "nick.ermolenkov@gmail.com", "Subject Line", "HTML Body Text");
+        //        }
+        //    }
+
+
+        //}
+        //    public override void ItemAdded(SPItemEventProperties properties)
+        //    {
+        //        string url = properties.WebUrl;
+
+        //        SPSecurity.RunWithElevatedPrivileges(delegate ()
+        //        {
+        //            using (SPSite site = new SPSite(url))
+        //            {
+        //                using (SPWeb web = site.OpenWeb())
+        //                {
+        //                    var success = SPUtility.SendEmail(web, true, true, "nick.ermolenkov@gmail.com", "Subject Line", "HTML Body Text");
+        //                    Console.WriteLine(success);
+        //                }
+        //            }
+        //        });
+        //    }
+        //}
+
+        public class UpdateEventReceiver : SPItemEventReceiver
+        {
+            public override void ItemUpdated(SPItemEventProperties properties)
+            {
+                base.ItemUpdated(properties);
+                //if (properties.ListItem["State"].ToString() == "Approved")
+                //{
+                //    string toField = "nick.ermolenkov@gmail.com";
+                //    var success = SPUtility.SendEmail(properties.Web, true, true, toField, "Subject Line", "HTML Body Text");
+                //}
+            }
+        }
+    }
+}
---
+++ Nickerm/VacationForm/VacationForm/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c355258e-16f6-4451-9c75-bb30364380df" featureId="c355258e-16f6-4451-9c75-bb30364380df" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="VacationForm.Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="1058cc5d-3fed-4dac-82d2-a8b846f9805d" />
+  </projectItems>
+</feature>
